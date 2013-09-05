d295eac041d519472af6d1421fc8a2fdb4db8014
pedro.mancheno@rga.com <pedro.mancheno@rga.com>
2013-09-05 11:43:58 -0300

    Initial Commit

---
+++ PMExtensions/PMExtensions.h
@@ -0,0 +1,13 @@
+//
+//  PMExtensions.h
+//  PMExtensions
+//
+//  Created by Pedro Mancheno on 9/5/13.
+//  Copyright (c) 2013 Pedro Mancheno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMExtensions : NSObject
+
+@end
---
+++ PMExtensionsTests/PMExtensionsTests.h
@@ -0,0 +1,13 @@
+//
+//  PMExtensionsTests.h
+//  PMExtensionsTests
+//
+//  Created by Pedro Mancheno on 9/5/13.
+//  Copyright (c) 2013 Pedro Mancheno. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PMExtensionsTests : SenTestCase
+
+@end
