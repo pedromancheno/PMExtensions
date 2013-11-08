088563aef464450431eabdf1a39d36273fef108a
pedro.mancheno@rga.com <pedro.mancheno@rga.com>
2013-11-08 15:57:23 -0300

    [Array Extensions] Add remainder of helper methods

--- PMExtensions/NSArray+PM.h
+++ PMExtensions/NSArray+PM.h
@@ -52,4 +52,86 @@
  */
 - (NSArray *)select:(BOOL (^)(id object))block;
 
+/**
+ * Executes the block for each element in the array and returns the first object in self for which
+ * the block is true. Return nil if no match is found.
+ * 
+ * @param block     Block to be called for each element in the array
+ *
+ * @return          First object contained in self for which the block returns YES.
+ */
+- (id)match:(BOOL (^)(id obj))block;
+
+/*!
+ Returns YES if the block is true for all objects in self.
+ */
+- (BOOL)all:(BOOL (^)(id obj))block;
+
+/*!
+ Returns YES if the block is true for any object in self.
+ */
+- (BOOL)any:(BOOL (^)(id obj))block;
+
+/*!
+ Invokes block once for each element of self, returning a dictionary with these elements
+ as values and the results of the block invocations as keys.
+ */
+- (NSDictionary*)mapToDictionary:(id<NSCopying> (^)(id obj))block;
+
+/*!
+ Invokes block once for each element of self, passing an index and returning a dictionary with these elements
+ as values and the results of the block invocations as keys.
+ */
+- (NSDictionary*)mapToDictionaryWithIndexes:(id<NSCopying> (^)(id obj, NSUInteger index))block;
+
+/*!
+ Returns a new array obtained by removing from the receiver all objects that are not in the given array.
+ */
+- (NSArray *)intersect:(NSArray*)array;
+
+/*!
+ Returns a new array with the receiver's elements in reverse order.
+ */
+- (NSArray *)reversed;
+
+/*!
+ Returns a copy of the array with all objects that are NSNull instances removed.
+ */
+- (NSArray*)arrayByRemovingNulls;
+
+/*!
+ Returns a new array that is a copy of the receiving array with the given object removed.
+ */
+- (NSArray*)arrayByRemovingObject:(id)object;
+
+/*!
+ Returns a new array that is a copy of the receiving array with the given object removed.
+ */
+- (NSArray*)arrayByRemovingObjectsInArray:(NSArray*)otherArray;
+
+/*!
+ Returns a new array that is a copy of the receiving array with the given object inserted.
+ */
+- (NSArray*)arrayByInsertingObject:(id)object atIndex:(NSUInteger)index;
+
+/*!
+ Returns a new array containing the receiving array’s elements up to a specified count.
+ */
+- (NSArray*)subarrayWithCount:(NSUInteger)count;
+
+/*!
+ Returns a new array containing the receiving array’s elements up to a specified index.
+ */
+- (NSArray*)subarrayToIndex:(NSUInteger)idx;
+
+/*!
+ Returns a new array containing the receiving array’s elements starting from a specified index.
+ */
+- (NSArray*)subarrayFromIndex:(NSInteger)idx;
+
+/*!
+ Returns a copy of the receiving array sorted by using a given key path for comparison.
+ */
+- (NSArray *)sortedArrayUsingKey:(NSString*)key ascending:(BOOL)ascending;
+
 @end
--- PMExtensions/NSArray+PM.m
+++ PMExtensions/NSArray+PM.m
@@ -45,4 +45,109 @@
     }];
 }
 
+- (id)match:(BOOL (^)(id obj))block
+{
+    for (id obj in self) {
+        if (block(obj)) {
+            return obj;
+        }
+    }
+    return nil;
+}
+
+- (id)reduce:(id)initial block:(id (^)(id accumulator, id obj))block
+{
+    id accumulator = initial;
+    for (id obj in self) {
+        accumulator = block(accumulator, obj);
+    }
+    return accumulator;
+}
+
+- (BOOL)all:(BOOL (^)(id obj))block
+{
+    BOOL (^negatedBlock)(id obj) = ^BOOL(id obj) {
+        return !block(obj);
+    };
+    return [self match:negatedBlock] == nil;
+}
+
+- (BOOL)any:(BOOL (^)(id obj))block
+{
+    return [self match:block] != nil;
+}
+
+- (NSDictionary*)mapToDictionaryWithIndexes:(id<NSCopying> (^)(id obj, NSUInteger index))block
+{
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:[self count]];
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        
+        id<NSCopying> key = block(obj, idx);
+        if (key)
+            dict[key] = obj;
+    }];
+    
+    return dict;
+}
+
+#pragma mark -
+
+- (NSArray*)arrayByRemovingNulls
+{
+    return [self select:^BOOL(id obj) {
+        return obj != [NSNull null];
+    }];
+}
+
+- (NSArray*)arrayByRemovingObject:(id)object
+{
+    NSMutableArray *copy = [self mutableCopy];
+    [copy removeObject:object];
+    return [NSArray arrayWithArray:copy];
+}
+
+- (NSArray*)arrayByRemovingObjectsInArray:(NSArray*)otherArray
+{
+    NSMutableArray *copy = [self mutableCopy];
+    [copy removeObjectsInArray:otherArray];
+    return [NSArray arrayWithArray:copy];
+}
+
+- (NSArray*)arrayByInsertingObject:(id)object atIndex:(NSUInteger)index
+{
+    NSMutableArray *copy = [self mutableCopy];
+    [copy insertObject:object atIndex:index];
+    return [NSArray arrayWithArray:copy];
+}
+
+- (NSArray*)subarrayWithCount:(NSUInteger)count
+{
+    return [self subarrayWithRange:(NSRange){
+        0, MIN(count, self.count)
+    }];
+}
+
+- (NSArray*)subarrayToIndex:(NSUInteger)idx
+{
+    return [self subarrayWithCount:idx+1];
+}
+
+- (NSArray*)subarrayFromIndex:(NSInteger)idx
+{
+    if (idx == NSNotFound || idx > ((NSInteger)self.count)-1)
+        return @[];
+    
+    return [self subarrayWithRange:(NSRange){
+        idx, self.count-idx
+    }];
+}
+
+- (NSArray *)sortedArrayUsingKey:(NSString*)key ascending:(BOOL)ascending
+{
+    return [self sortedArrayUsingDescriptors:@[
+                                               [NSSortDescriptor sortDescriptorWithKey:key ascending:ascending]
+                                               ]];
+}
+
 @end
