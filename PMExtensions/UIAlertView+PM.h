e10525b021147692e638c6eee4e17b99f70a0ee8
Pedro Mancheno <pedromancheno@gmail.com>
2015-03-02 17:38:06 +0100

    Add UIAlert convenient method to show alert

---
+++ PMExtensions/UIAlertView+PM.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+PM.h
+//  PMExtensions
+//
+//  Created by Pedro Mancheno on 02/03/15.
+//  Copyright (c) 2015 Pedro Mancheno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (PM)
+
+@end
---
+++ PMExtensions/UIAlertView+PM.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+PM.m
+//  PMExtensions
+//
+//  Created by Pedro Mancheno on 02/03/15.
+//  Copyright (c) 2015 Pedro Mancheno. All rights reserved.
+//
+
+#import "UIAlertView+PM.h"
+
+@implementation UIAlertView (PM)
+
+@end
