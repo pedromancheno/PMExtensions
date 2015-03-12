e567860529cdc8109c1ba3cc88f5cbc5d64b461a
Pedro Mancheno <pedromancheno@gmail.com>
2015-03-12 16:38:25 +0100

    Clean up

--- PMExtensions/UIAlertView+PM.h
+++ PMExtensions/UIAlertView+PM.h
@@ -6,8 +6,25 @@
 //  Copyright (c) 2015 Pedro Mancheno. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+typedef void (^UIAlertViewDismissBlock)(NSUInteger buttonIndex);
+typedef void (^UIAlertViewCancelBlock)();
 
-@interface UIAlertView (PM)
+@interface UIAlertView (PM) <UIAlertViewDelegate>
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message;
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle;
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle
+                  otherButtonTitles:(NSArray *)otherButtons
+                          onDismiss:(UIAlertViewDismissBlock)dismissed
+                           onCancel:(UIAlertViewCancelBlock)cancelled;
+
++ (UIAlertView *)alertViewWithError:(NSError *)error;
 
 @end
--- PMExtensions/UIAlertView+PM.m
+++ PMExtensions/UIAlertView+PM.m
@@ -8,6 +8,73 @@
 
 #import "UIAlertView+PM.h"
 
+static UIAlertViewDismissBlock _dismissBlock;
+static UIAlertViewCancelBlock _cancelBlock;
+
 @implementation UIAlertView (PM)
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle
+                  otherButtonTitles:(NSArray *)otherButtons
+                          onDismiss:(UIAlertViewDismissBlock)dismissed
+                           onCancel:(UIAlertViewCancelBlock)cancelled
+{
+	_cancelBlock  = [cancelled copy];
+	_dismissBlock  = [dismissed copy];
+
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+	                                                message:message
+	                                               delegate:[self class]
+	                                      cancelButtonTitle:cancelButtonTitle
+	                                      otherButtonTitles:nil];
+
+	for (NSString *buttonTitle in otherButtons)
+		[alert addButtonWithTitle:buttonTitle];
+
+	[alert show];
+	return alert;
+}
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+{
+	return [UIAlertView alertViewWithTitle:title
+	                               message:message
+	                     cancelButtonTitle:NSLocalizedString(@"Dismiss", @"")];
+}
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle
+{
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+	                                                message:message
+	                                               delegate:nil
+	                                      cancelButtonTitle:cancelButtonTitle
+	                                      otherButtonTitles:nil];
+	[alert show];
+	return alert;
+}
+
++ (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSUInteger)buttonIndex
+{
+	if (buttonIndex == [alertView cancelButtonIndex]) {
+		if (_cancelBlock) {
+			_cancelBlock();
+		}
+	}
+
+	else {
+		if (_dismissBlock) {
+			_dismissBlock(buttonIndex - 1); // cancel button is button 0
+		}
+	}
+}
+
++ (UIAlertView *)alertViewWithError:(NSError *)error
+{
+	return [self alertViewWithTitle:@"Error" message:error.localizedDescription];
+}
 
 @end
